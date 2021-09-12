0135b8fc6e0f40a4367ba734e638aa5d114acc5e
Sherbazkhan148 <sherbazkhan148@gmail.com>
2021-09-12 17:27:40 +0500

    Your Commit

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -20,7 +20,7 @@ const Header = () => {
             <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
                 <Container>
                     <LinkContainer to="/">
-                        <Navbar.Brand>Falak House</Navbar.Brand>
+                        <Navbar.Brand>A to Z Mobile</Navbar.Brand>
                     </LinkContainer>
 
                     <Route
